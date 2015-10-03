5214718c3b033cfa6bf125af41a4f7b08faa95c3
Jon Topielski <polkorame@gmail.com>
2015-10-03 16:29:16 -0700

    Save all files from the 2 hour mentor help. Just in case.

--- popup.js
+++ popup.js
@@ -1,12 +1,50 @@
 function getJSON(callback){
+	
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if(xhttp.readyState == 4 && xhttp.status == 200){
 			callback(xhttp.responseText);
 		}
 	}
-	xhttp.open("GET", "http://52.25.136.126:5984/craigslist/3b277a6d1cd211fc820edfce8b000e9f", true);
-	xhttp.send();
+
+	xhttp.open("POST", "http://52.25.136.126:5984/craigslist", true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.setRequestHeader("Access-Control-Allow-Methods","GET, PUT, POST, DELETE")
+	xhttp.setRequestHeader("Access-Control-Allow-Origin","*")
+	xhttp.send({"Hello": "test"});
+
+	//jQuery.support.cors = true;
+	/*jQuery.ajax({
+    //url: "http://52.25.136.126:5984/_utils/database.html?craigslist",
+    url: "http://52.25.136.126:5984/craigslist",
+    data: { "id":"doc1", "rows":"100" },
+    type: "POST",
+    timeout: 30000,
+    dataType: "json", // "xml", "json"
+    xhrFields: {
+       withCredentials: false
+    },
+    crossDomain: true
+    success: function(data) {
+        // show text reply as-is (debug)
+        //alert(data);
+        console.log(data)*/
+
+        // show xml field values (debug)
+        //alert( $(data).find("title").text() );
+
+        // loop JSON array (debug)
+        //var str="";
+        //$.each(data.items, function(i,item) {
+        //  str += item.title + "\n";
+        //});
+        //alert(str);
+    },
+    error: function(jqXHR, textStatus, ex) {
+       	console.log(textStatus + "," + ex + "," + jqXHR.responseText);
+    }
+});
+
 	//xhttp.open("PUT", "http://52.25.136.126:5984/craigslist", true);
 	//xhttp.send(JSON.parse({"Test":"1,2,3"}));
 }
@@ -38,11 +76,12 @@ function myFunction(tablink) {
 	alert(tablink);
 }
 
+/*
 function btn()
 {
-  //alert(tab);
-  console.log(tab);
-}
+  alert(tab);
+  //console.log(tab);
+}*/
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('Save').addEventListener('click', btn);
