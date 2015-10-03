6d18608467fe8497eab006be4fa637b9b9ca4e8b
Evan Doyle <evanmdoyle@gmail.com>
2015-10-03 12:01:25 -0700

    Successfully retriving and parsing JSON from couchDB using xhttp

--- popup.js
+++ popup.js
@@ -1,17 +1,16 @@
-/*var xhttp = new XMLHttpRequest();
-if('withCredentials' in xhttp){
-	xhttp.open('GET', 'http://52.25.136.126:5984/_utils/document.html?craigslist/3b277a6d1cd211fc820edfce8b000e9f'
-	, true);
+function getJSON(callback){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if(xhttp.readyState == 4 && xhttp.status == 200){
+			callback(xhttp.responseText);
+		}
+	}
+	xhttp.open("GET", "http://52.25.136.126:5984/craigslist/3b277a6d1cd211fc820edfce8b000e9f", true);
 	xhttp.send();
-	alert(xhttp.responseText);
-}*/
-
-chrome.tabs.getSelected(null, function(tab){
-    myFunction(tab.url);
-});
-
-function myFunction(tablink) {
-
-	alert(tablink);
-
 }
+
+getJSON(function processJSON(data){
+	var processedData = JSON.parse(data);
+	alert("URL: " + processedData.url + " Price: " + processedData.price + " Location: "
+		+ processedData.location);
+});
