eb8e0e11a9f7fc1828dcb35a1e21501a24378efd
Caelean Barnes <caeleanb@gmail.com>
2015-10-03 10:54:14 -0700

    working html scraper

--- backend/pulldata.js
+++ backend/pulldata.js
@@ -1,69 +1,77 @@
 
+jQuery.ajax = (function(_ajax){
+
+    var protocol = location.protocol,
+        hostname = location.hostname,
+        exRegex = RegExp(protocol + '//' + hostname),
+        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
+        query = 'select * from html where url="{URL}" and xpath="*"';
+
+    function isExternal(url) {
+        return !exRegex.test(url) && /:\/\//.test(url);
+    }
+
+    return function(o) {
+
+        var url = o.url;
+
+        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
+
+            // Manipulate options so that JSONP-x request is made to YQL
+
+            o.url = YQL;
+            o.dataType = 'json';
+
+            o.data = {
+                q: query.replace(
+                    '{URL}',
+                    url + (o.data ?
+                        (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
+                    : '')
+                ),
+                format: 'xml'
+            };
+
+            // Since it's a JSONP request
+            // complete === success
+            if (!o.success && o.complete) {
+                o.success = o.complete;
+                delete o.complete;
+            }
+
+            o.success = (function(_success){
+                return function(data) {
+
+                    if (_success) {
+                        // Fake XHR callback.
+                        _success.call(this, {
+                            responseText: data.results[0]
+                                // YQL screws with <script>s
+                                // Get rid of them
+                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
+                        }, 'success');
+                    }
+
+                };
+            })(o.success);
+
+        }
+
+        return _ajax.apply(this, arguments);
+
+    };
+
+})(jQuery.ajax);
+
 function getData() {
   var url = document.getElementById("url").value;
-  // $.get("?u=http://www.craigslist.org/about/sites", function(response) { alert(response) });
-  var data = $.getJSON('http://query.yahooapis.com/v1/public/yql?q=select * from html where url="http://sandiego.craigslist.org/"&format=json');
-  console.log(data);
-  alert(JSON.stringify(data));
-  // makeCorsRequest(url);
-  // $.ajax({
-  //    url: "http://google.com",
-  //    type: "GET",
-  //    dataType: 'jsonp',
-  //    headers: { 'Access-Control-Allow-Origin': 'http://google.com',
-  //               'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'},
-  //    success: function(data) {
-  //         console.log("worked");
-  //         // var elements = $("<div>").html(data)[0].getElementsByTagName("ul")[0].getElementsByTagName("li");
-  //         // for(var i = 0; i < elements.length; i++) {
-  //         //      var theText = elements[i].firstChild.nodeValue;
-  //         //      // Do something here
-  //         // }
-  //    }
-// });
- }
-// function createCORSRequest(method, url) {
-//   var xhr = new XMLHttpRequest();
-//   if ("withCredentials" in xhr) {
-//     // XHR for Chrome/Firefox/Opera/Safari.
-//     xhr.open(method, url, true);
-//   } else if (typeof XDomainRequest != "undefined") {
-//     // XDomainRequest for IE.
-//     xhr = new XDomainRequest();
-//     xhr.open(method, url);
-//     xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-//   } else {
-//     // CORS not supported.
-//     xhr = null;
-//   }
-//   return xhr;
-// }
-//
-// // Helper method to parse the title tag from the response.
-// function getTitle(text) {
-//   return text.match('<title>(.*)?</title>')[1];
-// }
-//
-// // Make the actual CORS request.
-// function makeCorsRequest(address) {
-//   // All HTML5 Rocks properties support CORS.
-//   var url = 'http://cors.io/?u=' + address;
-//
-//   var xhr = createCORSRequest('GET', url);
-//   if (!xhr) {
-//     alert('CORS not supported');
-//     return;
-//   }
-//
-//   // Response handlers.
-//   xhr.onload = function() {
-//     var text = xhr.responseText;
-//     alert('Response from CORS request to ' + url + ': ' + text);
-//   };
-//
-//   xhr.onerror = function() {
-//     // alert('Woops, there was an error making the request.');
-//   };
-//
-//   xhr.send();
-// }
+  $.ajax({
+      url: url,
+      type: 'GET',
+      success: function(res) {
+          var text = res.responseText;
+          console.log(res);
+          alert(text);
+      }
+  });
+}
