d3d7bf2da02b496ed9bc50e4253bf7db87787d5b
anmolm96 <anmol.maini@gmail.com>
2015-10-03 00:42:46 -0700

    Added Shit

--- popup.js
+++ popup.js
@@ -1,27 +1,9 @@
-function getCurrentTabUrl(callback){
-	var queryInfo = {
-		active: true,
-		currentWindow: true
-	};
+chrome.tabs.getSelected(null, function(tab) {
+    myFunction(tab.url);
+});
 
-	//queryInfo is the set of properties that the tabs must match
-	//the matching tabs are sent to the callback
-	chrome.tabs.query(queryInfo, function(tabs)){
-		var tab = tabs[0];
-		var url = tab.url;
-		console.assert(typeof url == 'string', 'tab.url should be a string');
-
-		callback(url);
-	}
+function myFunction(tablink) {
+  // do stuff here
+  console.log(tablink);
+	alert(tablink);
 }
-
-getCurrentTabUrl(function storeURL(url){
-	var tabTable = {};
-
-	tabTable["first"] = url;
-	storage.set(tabTable);
-});
-
-storage.get(tabTable, function(result){
-	alert(result["first"]);
-});
