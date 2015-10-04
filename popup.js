65508df2240cf41815cc53ba08874abc15d45007
Evan Doyle <evanmdoyle@gmail.com>
2015-10-03 18:22:17 -0700

    Merging popup

--- popup.js
+++ popup.js
@@ -1,22 +1,3 @@
-function getJSON(callback){
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if(xhttp.readyState == 4 && xhttp.status == 200){
-			callback(xhttp.responseText);
-		}
-	}
-	xhttp.open("GET", "http://52.25.136.126:5984/craigslist/3b277a6d1cd211fc820edfce8b000e9f", true);
-	xhttp.send();
-}
-
-getJSON(function processJSON(data)
-{
-	var processedData = JSON.parse(data);
-	//console.log("URL: " + processedData.url + " Price: " + processedData.price +
-		//" Location: "+ processedData.location);
-});
-
-
 var tab;
 
 function myFunction(tablink) {
